fee1f80270b85585a4122c3d424e30e8c8fc9996
bakowroc <kontakt@maciejbakowicz.pl>
2017-10-12 00:29:57 +0200

    Refactor and cleanup

--- server/utils/APIRequest.ts
+++ server/utils/APIRequest.ts
@@ -5,51 +5,40 @@ import { ResponseError, ResponseHandler } from '../types/typings';
 
 class APIRequest {
 
-  public GET_MANY<T>(DataModel: Model<any>): ResponseHandler {
-    const RESPONSE_HANDLER = (request: Request, response: Response): void => {
+  public GET_MANY = <T>(DataModel: Model<any>): ResponseHandler =>
+    (request: Request, response: Response): void => {
       DataModel.find({})
-        .then((data: Array<T>) => this.getJSONResponse(response, data))
-        .catch((error: ResponseError) => this.getJSONResponse(response, error));
-    };
+        .then((data: Array<T>) => this.JSONResponse(response, data))
+        .catch((error: ResponseError) => this.JSONResponse(response, error));
+    }
 
-    return RESPONSE_HANDLER;
-  }
-
-  public GET_SINGLE<T>(DataModel: Model<any>): ResponseHandler {
-    const RESPONSE_HANDLER = (request: Request, response: Response): void => {
+  public GET_SINGLE = <T>(DataModel: Model<any>): ResponseHandler => 
+    (request: Request, response: Response): void => {
       const slug: number = request.params.slug;
       DataModel.findOne({slug})
-        .then((data: T) => this.getJSONResponse(response, data))
-        .catch((error: ResponseError) => this.getJSONResponse(response, error));
-    };
+        .then((data: T) => this.JSONResponse(response, data))
+        .catch((error: ResponseError) => this.JSONResponse(response, error));
+    }
 
-    return RESPONSE_HANDLER;
-  }
+  public POST = <T>(DataModel: Model<any>, getBodyByRules?: (body: T) => T): ResponseHandler => 
+    (request: Request, response: Response): void => {
+      const DataModelBody = arguments.length === 2 ? getBodyByRules(request.body) : request.body;
+      new DataModel(DataModelBody).save()
+        .then((data: T) => this.JSONResponse(response, data))
+        .catch((error: ResponseError) => this.JSONResponse(response, error));
+    }
 
-  public POST<T>(DataModel: Model<any>, rules?: (body: T) => T): ResponseHandler {
-    const RESPONSE_HANDLER = (request: Request, response: Response): void => {
+  public UPDATE = <T>(DataModel: Model<any>, rules?: (body: T) => T): ResponseHandler =>
+    (request: Request, response: Response): void => {
       const DataModelBody = arguments.length === 2 ? rules(request.body) : request.body;
       new DataModel(DataModelBody).save()
-        .then((data: T) => this.getJSONResponse(response, data))
-        .catch((error: ResponseError) => this.getJSONResponse(response, error));
-    };
+        .then((data: T) => this.JSONResponse(response, data))
+        .catch((error: ResponseError) => this.JSONResponse(response, error));
+    }
 
-    return RESPONSE_HANDLER;
-  }
-
-  public UPDATE<T>(DataModel: Model<any>, rules?: (body: T) => T): ResponseHandler {
-    const RESPONSE_HANDLER = (request: Request, response: Response): void => {
-      const DataModelBody = arguments.length === 2 ? rules(request.body) : request.body;
-      new DataModel(DataModelBody).save()
-        .then((data: T) => this.getJSONResponse(response, data))
-        .catch((error: ResponseError) => this.getJSONResponse(response, error));
-    };
-
-    return RESPONSE_HANDLER;
-  }
-
-  private getJSONResponse<T>(requestResponse: Response, responseData: T): void {
+  private JSONResponse<T>(requestResponse: Response, responseData: T): void {
     const status: number = requestResponse.statusCode;
+
     requestResponse.json({
       responseData,
       status
